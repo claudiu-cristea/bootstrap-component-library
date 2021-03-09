127a547ca1179c2074c2e9764eb4746b2c8a8fde
planctus <davidepuntoferrante@gmail.com>
2021-03-09 20:40:02 +0200

    feat(components): adding breadcrumb

---
+++ src/components/breadcrumb/breadcrumb.story.js
@@ -0,0 +1,9 @@
+import badge from "./breadcrumb.html.twig";
+
+export default {
+  title: "Components/Breadcrumb",
+};
+
+export const Breadcrumb = (args) => badge(args);
+
+Breadcrumb.storyName = "default";
